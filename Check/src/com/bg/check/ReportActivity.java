a00f6d2db5672f06b107067d6d0bccdbf9807109
Tsung <username@email.com>
2012-07-26 23:50:22 +0800

    add SelectReprotActivity and ReportActivity

---
+++ Check/src/com/bg/check/ReportActivity.java
@@ -0,0 +1,20 @@
+package com.bg.check;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ReportActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_home, menu);
+        return true;
+    }
+}
