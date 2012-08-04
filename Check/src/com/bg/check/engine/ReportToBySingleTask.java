61bd6e1fa56ebce748e649f301c895921bca6363
Tsung <username@email.com>
2012-08-05 01:03:26 +0800

    Complete other 4 tasks

--- Check/src/com/bg/check/engine/ReportToBySingleTask.java
+++ Check/src/com/bg/check/engine/ReportToBySingleTask.java
@@ -1,16 +1,49 @@
 
 package com.bg.check.engine;
 
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+import com.bg.check.webservice.SCWebService;
+
 public class ReportToBySingleTask extends BaseTask {
+    private String mUserDM;
+
+    private String mReport;
+
+    private ReportToBySingleTask(String dm, String report) {
+        mUserDM = dm;
+        mReport = report;
+    }
 
     /**
-     * Return Login status with Integer Value
-     * 1 : success
-     * 0 : fail
+     * Return ReplyTasks status with Integer Value 1 : success 0 : fail
      */
     @Override
     public Object run() {
-        return 1;
+        SoapObject rpc = new SoapObject(SCWebService.SC_NAME_SPACE,
+                SCWebService.SC_METHOD_REPORT_TO_BY_SINGLE);
+
+        rpc.addProperty("User_dm", mUserDM);
+        rpc.addProperty("report", mReport);
+
+        SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER10);
+
+        envelope.bodyOut = rpc;
+        envelope.dotNet = true;
+        envelope.setOutputSoapObject(rpc);
+
+        HttpTransportSE transport = new HttpTransportSE(SCWebService.SC_END_POINT);
+        try {
+            transport.call(SCWebService.SC_NAME_SPACE + SCWebService.SC_METHOD_REPORT_TO_BY_SINGLE,
+                    envelope);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        SoapObject object = (SoapObject)envelope.bodyIn;
+        return Integer.parseInt(object.getProperty(0).toString());
     }
 
 }
